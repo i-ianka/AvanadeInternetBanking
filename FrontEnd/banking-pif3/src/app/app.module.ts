ebece29f25a3ccb449b9265402fda68effabb22b
lloma93 <palloma.adolfi@gmail.com>
2018-12-05 13:45:17 -0200

    componentes iniciais

--- FrontEnd/banking-pif3/src/app/app.module.ts
+++ FrontEnd/banking-pif3/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { CustomerServiceComponent } from './components/customer-service/customer-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    CustomerServiceComponent
   ],
   imports: [
     BrowserModule
